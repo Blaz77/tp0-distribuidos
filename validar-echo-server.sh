5c6bb9390ea258a11f021a5a43989d421fce4c77
Lucas Soro <lsoro@fi.uba.ar>
2026-03-14 23:17:33 -0300

    Add echo server validation script

---
+++ validar-echo-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER_CONTAINER_NAME="server"
+SERVER_PORT=4806
+NETWORK_NAME="tp0_testing_net"
+
+TEST_STRING="testing_the_server"
+
+# Using alpine because it's lightweight but has netcat
+RESULT=$(docker run --rm --network $NETWORK_NAME alpine sh -c "echo $TEST_STRING | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
+
+if [ "$RESULT" = "$TEST_STRING" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
